53be68a377680bcffbb27ea755c624a23d368aef
Raghavendra Dattawad <raghavendra.d@wickedride.com>
2018-06-11 01:10:48 -0700

    first commi

---
+++ AlamofireDemo/Model.swift
@@ -0,0 +1,37 @@
+//
+//  Model.swift
+//  AlamofireDemo
+//
+//  Created by Raghavendra Dattawad on 12/23/17.
+//  Copyright © 2017 Raghavendra Dattawad. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+
+class Model: NSObject {
+    
+    
+//    var contacts:Array<String>?
+//    
+//    
+//    init(nameDetails:Array<AnyObject>?){
+//        
+//        
+//        
+//        
+//    }
+
+    var name:String?
+    
+    init(getName:String){
+        
+        
+        name = getName
+        
+    }
+    
+    
+
+}
+
--- AlamofireDemo/ViewController.swift
+++ AlamofireDemo/ViewController.swift
@@ -7,19 +7,85 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
-class ViewController: UIViewController {
 
+
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
+{
+
+@IBOutlet weak var tableView: UITableView!
+    
+    
+    var  contactNames = [String]()
+    var contactEmailAddreess = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+ 
+        Alamofire.request("http://api.androidhive.info/contacts").responseJSON {
+            (responsseData) -> Void in
+            
+            
+            if responsseData.result.isSuccess
+    {
+                let resJson = JSON(responsseData.result.value!)
+                
+                
+         let json =       JSON (resJson["contacts"])
+               
+                
+       for conNamesDetails in json.array! {
+        
+        let nameString:String? = conNamesDetails["name"].stringValue
+        let emailAddress:String? = conNamesDetails["email"].stringValue
+        
+        
+        
+        /// append array //
+        self.contactNames.append(nameString!)
+        self.contactEmailAddreess.append(emailAddress!)
+        
+            }
+            
+                
+       self.tableView.reloadData()
+         
+            
     }
-
+            if responsseData.result.isFailure {
+                let error : NSError = responsseData.result.error! as NSError
+            print(error)
+            }
+        }
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Dispose of any resources that can be recreated
+        
+        
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.contactNames.count;
+      
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        
+        cell.textLabel?.text = self.contactNames[indexPath.row]
+        
+        cell.detailTextLabel?.text = self.contactEmailAddreess[indexPath.row]
+        return cell
+        
+    }
+    
 }
 
